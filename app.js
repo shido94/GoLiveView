e8b697ffbf8b9036c0bc707a24d313d1c94456e9
shido94 <rupeshyadav94.ry@gmail.com>
2019-04-06 00:58:22 +0530

    Second commit

--- app.js
+++ app.js
@@ -3,21 +3,15 @@ const http = require('http');
 const url = require('url');
 const bodyParser = require('body-parser');
 const path = require('path');
-const mongoose = require('mongoose');
-const fs = require('fs');
-const ss = require('socket.io-stream');
+
 
 channels = {};
 sockets = {};
-
-
 
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 
-
-// const host = '192.168.10.126';
 const port = 3000;
 
 
@@ -25,102 +19,109 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/dist/project'));
+app.use(express.static(__dirname + '/dist/GoLiveView'));
+
 
 
 
 
 io.on('connection', (socket)  => {
-    socket.channels = {};
-    sockets[socket.id] = socket;
-    console.log("[ " + socket.id + " ] connection accepted");
+  socket.channels = {};
+  sockets[socket.id] = socket;
+  console.log("[ " + socket.id + " ] connection accepted");
 
-    socket.on('disconnect', () => {
-        // console.log('reaches');
-        for(let channel in socket.channels) {
-            console.log('channel', channel);
-            part(channel);
-        }
+  socket.on('disconnect', () => {
+    for(let channel in socket.channels) {
+      console.log('channel', channel);
+      part(channel);
+    }
+    delete socket[socket.id];
 
-        delete socket[socket.id];
+  });
 
-    });
+  socket.on('pageDisconnected', () => {
+    for(let channel in socket.channels) {
+      console.log('channel', channel);
+      part(channel);
+    }
+    delete socket[socket.id];
+  });
 
-    socket.on('check', (val) => {
-        // console.log(val);
-    });
+  socket.on('check', (val) => {
+    // console.log(val);
+  });
 
-    socket.on('join', (config) => {
-        // console.log("["+ socket.id + "] join ", config);
-        const channel = config.channel;
-        const userdata = config.userdata;
-        if(channel in socket.channels) {
-            console.log("["+ socket.id + "] ERROR: already joined ", channel);
-            return;
-        }
-        if(!(channel in channels)) {
-            channels[channel] = {};
-        }
-        for (let id in channels[channel]) {
-            console.log('Id => ', id);
-            // console.log(socket.id);
-            channels[channel][id].emit('addPeer', {peerId: socket.id, should_create_offer: false});
-            socket.emit('addPeer', {peerId: id, should_create_offer: true});
-        }
-
-        channels[channel][socket.id] = socket;
-        socket.channels[channel] = channel;
-    });
-
-    function part(channel) {
-        // console.log("[ "+ socket.id + " ] part");
-
-        if(!(channel in socket.channels)) {
-            // console.log("["+ socket.id + "] ERROR: not in ", channel);
-            return;
-        }
-
-        delete socket.channels[channel];
-        delete channels[channel][socket.id];
-
-        for( id in channels[channel]) {
-            channels[channel][id].emit('removePeer', {'peerId' : socket.id});
-            socket.emit('removePeer', {'peerId' : id});
-        }
+  socket.on('join', (config) => {
+    // console.log("["+ socket.id + "] join ", config);
+    const channel = config.channel;
+    const userdata = config.userdata;
+    if(channel in socket.channels) {
+      // console.log("["+ socket.id + "] ERROR: already joined ", channel);
+      return;
+    }
+    if(!(channel in channels)) {
+      channels[channel] = {};
+    }
+    for (let id in channels[channel]) {
+      // console.log('Id => ', id);
+      // console.log(socket.id);
+      channels[channel][id].emit('addPeer', {peerId: socket.id, should_create_offer: false});
+      socket.emit('addPeer', {peerId: id, should_create_offer: true});
     }
 
-    socket.on('relayIceCandidate', (config) => {
-        // console.log(config);
-        const peerId = config.peerId;
-        const iceCandidate = config.iceCandidate;
-        // console.log("["+ socket.id + "] relaying ICE candidate to [" + peer_id + "] ", ice_candidate);
+    channels[channel][socket.id] = socket;
+    socket.channels[channel] = channel;
+  });
 
-        if  (peerId in sockets) {
-            sockets[peerId].emit('iceCandidates', {'peerId': socket.id, 'iceCandidate': iceCandidate});
-        }
-    });
+  function part(channel) {
+    console.log("[ "+ socket.id + " ] part");
 
-    socket.on('relaySessionDescription', (config) => {
-        const peerId = config.peerId;
-        const sessionDescription = config.sessionDescription;
-        // console.log("["+ socket.id + "] relaying session description to [" + peer_id + "] ", session_description);
+    if(!(channel in socket.channels)) {
+      // console.log("["+ socket.id + "] ERROR: not in ", channel);
+      return;
+    }
 
-        if(peerId in sockets) {
-            sockets[peerId].emit('sessionDescription', {'peerId': socket.id, 'sessionDescription': sessionDescription});
-        }
+    delete socket.channels[channel];
+    delete channels[channel][socket.id];
 
-    });
+    for( id in channels[channel]) {
+      channels[channel][id].emit('removePeer', {'peerId' : socket.id});
+      socket.emit('removePeer', {'peerId' : id});
+    }
+  }
+
+  socket.on('relayIceCandidate', (config) => {
+    // console.log(config);
+    const peerId = config.peerId;
+    const iceCandidate = config.iceCandidate;
+    // console.log("["+ socket.id + "] relaying ICE candidate to [" + peerId + "] ", iceCandidate);
+
+    if  (peerId in sockets) {
+      // console.log('peerId ', socket.id);
+      sockets[peerId].emit('iceCandidates', {'peerId': socket.id, 'iceCandidate': iceCandidate});
+    }
+  });
+
+  socket.on('relaySessionDescription', (config) => {
+    const peerId = config.peerId;
+    const sessionDescription = config.sessionDescription;
+    // console.log("["+ socket.id + "] relaying session description to [" + peerId + "] ", sessionDescription);
+    // console.log('peerId ', peerId);
+    // console.log('socketId', socket.id);
+    if(peerId in sockets) {
+      sockets[peerId].emit('sessionDescription', {'peerId': socket.id, 'sessionDescription': sessionDescription});
+    }
+
+  });
 
 
 });
 
-
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/project/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/GoLiveView/index.html'));
 });
 
 
 server.listen(port, () =>{
-    console.log(`Server running at port %d`   , port);
+  console.log('Server running at port ', port);
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MainComponent} from './main/main.component';
+import {VideochatComponent} from './videochat/videochat.component';
+import {ShowvideosComponent} from './showvideos/showvideos.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {routes} from './app.routes';
+import {ChatService} from './app.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VideochatComponent,
+    MainComponent,
+    ShowvideosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
